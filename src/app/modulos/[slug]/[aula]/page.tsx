6670a534f005baf246510aa601e2404d42b20b12
Victor Mortari <victorgmortari2@gmail.com>
2025-08-26 02:42:29 +0000

    aumente para 90x90px

--- src/app/modulos/[slug]/[aula]/page.tsx
+++ src/app/modulos/[slug]/[aula]/page.tsx
@@ -28,8 +28,8 @@ export default function LessonPage({ params }: { params: { slug: string; aula: s
             <Image 
                 src="https://i.imgur.com/VJtWx2S.png" 
                 alt="Diástase Zero Logo" 
-                width={70} 
-                height={70}
+                width={90} 
+                height={90}
                 className='mx-auto'
             />
           </Link>
--- src/app/modulos/[slug]/page.tsx
+++ src/app/modulos/[slug]/page.tsx
@@ -40,8 +40,8 @@ export default function ModulePage({ params }: { params: { slug: string } }) {
             <Image 
                 src="https://i.imgur.com/VJtWx2S.png" 
                 alt="Diástase Zero Logo" 
-                width={70} 
-                height={70}
+                width={90} 
+                height={90}
                 className='mx-auto'
             />
           </Link>
