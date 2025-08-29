b20b6d2deb7df8b21eb12d19ec1217f3efeb744e
Victor Mortari <victorgmortari2@gmail.com>
2025-08-29 14:06:59 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="antialiased">
         {children}
         <Toaster />
       </body>
