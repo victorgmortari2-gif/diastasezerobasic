5e46d3682418fe45494d9931dd21a6e0b9ef72fd
Victor Mortari <victorgmortari2@gmail.com>
2025-08-30 02:13:20 +0000

    deixe o site mais fluido e melhore a responsividade dele ao trocar as pá

---
+++ src/components/PageTransition.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { motion, AnimatePresence } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+
+const PageTransition = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.3,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+          },
+          animateState: {
+            opacity: 1,
+          },
+          exitState: {
+            opacity: 0,
+          },
+        }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default PageTransition;
